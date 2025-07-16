d78393ea49e3beb81c84c8f2d5c76077647696f8
ExXTreMe315 <112979320+ExXTreMe315@users.noreply.github.com>
2025-07-16 12:14:12 +0200

    Update fxmanifest.lua

--- platformResourcePresets/fiveM/fxmanifest.lua
+++ platformResourcePresets/fiveM/fxmanifest.lua
@@ -1,4 +1,9 @@
-description "myName"
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'John Doe <j.doe@example.com>'
+description 'Example resource'
+version '1.0.0'description "myName"
 version "1.0.0"
 
 fx_version "cerulean"
